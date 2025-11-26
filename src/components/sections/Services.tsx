62a79d09a57afb633036d8109681066f4be82dbf
Nafees <nafeesraza90121@gmail.com>
2025-11-26 12:13:12 +0000

    can we use iframe for that

--- src/components/sections/Services.tsx
+++ src/components/sections/Services.tsx
@@ -14,11 +14,14 @@ export default function Services({ services }: ServicesProps) {
       <div className="container">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6 justify-items-center">
           {services.map((service) => (
-            <div
-              key={service.id}
-              className="w-full"
-              dangerouslySetInnerHTML={{ __html: service.content }}
-            ></div>
+            <div key={service.id} className="w-full h-full">
+              <iframe
+                srcDoc={service.content}
+                className="w-full h-full border-0"
+                title={service.id}
+                sandbox="" // Added for extra security
+              />
+            </div>
           ))}
         </div>
       </div>
